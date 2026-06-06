f06d8bc334355f7da01c672e8bfd4b87de003794
Sujal Rathod <sujalr0712@gmail.com>
2026-06-06 17:07:15 +0530

    Fix URLs

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [ # HOME
@@ -61,3 +63,7 @@ path(
     name='add_review'
 ),
 ]
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
